7295803cb2949444bd4482e3d916709b78605bb0
JulieAnn Selman <julieannselman@gmail.com>
2023-03-08 14:15:57 -0500

    added faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,37 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require "open-uri"
+require "faker"
+
+user1 = User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  nickname: Faker::Superhero.name,
+  phone_number: Faker::PhoneNumber.cell_phone,
+  location: Faker::Address.country,
+  language: Faker::Nation.language,
+  email: "a@a.a",
+  password: "123456"
+)
+
+user2 = User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  nickname: Faker::Superhero.name,
+  phone_number: Faker::PhoneNumber.cell_phone,
+  location: Faker::Address.country,
+  language: Faker::Nation.language,
+  email: "b@b.b",
+  password: "123456"
+)
+
+user3 = User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  nickname: Faker::Superhero.name,
+  phone_number: Faker::PhoneNumber.cell_phone,
+  location: Faker::Address.country,
+  language: Faker::Nation.language,
+  email: "c@c.c",
+  password: "123456"
+)
