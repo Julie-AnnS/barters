1473561574c809d3d2d9f8d323ab5c3da0ff9373
JulieAnn Selman <julieannselman@gmail.com>
2023-03-08 14:34:11 -0500

    index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 require "open-uri"
 require "faker"
+
+puts "cleaning database"
+User.destroy_all
+
+
 
 user1 = User.create!(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
   nickname: Faker::Superhero.name,
   phone_number: Faker::PhoneNumber.cell_phone,
+  # phone_number: "5149991231",
   location: Faker::Address.country,
   language: Faker::Nation.language,
   email: "a@a.a",
