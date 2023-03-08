15cb93262f2da454850d0bf35ad128e7e915c9dc
Nilsu Duran <duranilsu@gmail.com>
2023-03-08 17:32:16 -0500

    added chat functionality, need to add active cable later

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,7 @@
 class ConversationsController < ApplicationController
   def show
     @conversation = Conversation.find(params[:id])
+    @message = Message.new
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'artists/:id', to: 'users#show', as: :artist
   get 'conversations/:id', to: 'conversations#show', as: :conversation
   post 'conversations', to: 'conversations#create'
-  post "/artists/:artist_id/messages", to: "messages#create", as: :messages
+  post 'conversations/:conversation_id/messages', to: "messages#create", as: :conversation_messages
 
   # get 'offers', to: 'offers#index'
 end
