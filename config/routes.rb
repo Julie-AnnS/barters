d7bf350c5f381a6ebc7404cb1062610da9e6196f
Nilsu Duran <duranilsu@gmail.com>
2023-03-09 11:57:01 -0500

    Restore deleted routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get 'artists', to: 'users#index'
   get 'artists/:id', to: 'users#show', as: :artist
   get 'offers', to: 'offers#index'
+  get 'conversations/:id', to: 'conversations#show', as: :conversation
+  post 'conversations', to: 'conversations#create'
+  post 'conversations/:conversation_id/messages', to: "messages#create", as: :conversation_messages
 end
