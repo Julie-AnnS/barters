857c8673e2568debe6dbf2fa1a0b000de4cc5438
Nilsu Duran <duranilsu@gmail.com>
2023-03-10 15:21:45 -0500

    still fixing migration

--- db/migrate/20230310024135_add_user_id_to_offers.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToOffers < ActiveRecord::Migration[7.0]
-  def change
-    execute ("DELETE FROM offers;")
-    add_reference :offers, :user, null: false, foreign_key: true
-  end
-end
--- db/migrate/20230310024552_change_user_column_in_offers.rb
+++
@@ -1,6 +0,0 @@
-class ChangeUserColumnInOffers < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :offers, :user, index: true, foreign_key: true
-    add_column :offers, :user, :string
-  end
-end
