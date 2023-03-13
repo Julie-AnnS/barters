fad79c7535d4b2ea71664424f92f4942e7b16d45
JulieAnn Selman <julieannselman@gmail.com>
2023-03-13 10:57:43 -0400

    added paragraphs at bottom, added Find your bARTers community

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,8 @@ class Offer < ApplicationRecord
   belongs_to :requester, class_name: "User"
   belongs_to :collaborator, class_name: "User"
 
+
+
   enum status: {
     pending: 0,
     accepted: 1,
