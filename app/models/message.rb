5ae6c924f8f720c4114b88f24c9710de4fc1a1a9
Nilsu Duran <duranilsu@gmail.com>
2023-03-14 16:49:06 -0400

    idk

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,17 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
+  belongs_to :recipient, class_name: "User"
+
+  after_create_commit :notify_recipient
 
   def sender?(a_user)
     user.id == a_user.id
+  end
+
+  private
+
+  def notify_recipient
+    NewMessageNotification.with(message: self).deliver(recipient)
   end
 end
---
+++ app/notifications/comment_notification.rb
@@ -0,0 +1,33 @@
+# To deliver this notification:
+#
+# CommentNotification.with(post: @post).deliver_later(current_user)
+# CommentNotification.with(post: @post).deliver(current_user)
+
+class CommentNotification < Noticed::Base
+  deliver_by :database
+  deliver_by :action_cable
+  # Add your delivery methods
+  #
+  # deliver_by :database
+  # deliver_by :email, mailer: "UserMailer"
+  # deliver_by :slack
+  # deliver_by :custom, class: "MyDeliveryMethod"
+
+  # Add required params
+  #
+  # param :post
+
+  # Define helper methods to make rendering easier.
+  #
+  def message
+    t(".message")
+  end
+
+  def url
+    post_path(params[:post])
+  end
+  
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  notification = CommentNotification.with(comment: @comment)
+  notification.deliver(@comment.post.author)
+end
