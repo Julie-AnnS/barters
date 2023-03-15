c3758f297b43278711ad2201dacf6513e06b6747
Nilsu Duran <duranilsu@gmail.com>
2023-03-15 11:48:25 -0400

    NOTIFICATION FOR MESSAGE WORKS

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,22 @@ class Message < ApplicationRecord
   def sender?(a_user)
     user.id == a_user.id
   end
+
+  after_create_commit :notify_recipient
+  before_destroy :cleanup_notifications
+  has_noticed_notifications model_name: "Notification"
+
+  private
+
+  def notify_recipient
+    if conversation.participant_one_id == user_id
+      MessageNotification.with(message: self, conversation: conversation).deliver_later(conversation.participant_two)
+    else
+      MessageNotification.with(message: self, conversation: conversation).deliver_later(conversation.participant_one)
+    end
+  end
+
+  def cleanup_notifications
+    notifications_as_message.destroy_all
+  end
 end
