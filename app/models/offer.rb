da408c3e520c3f3b7231eded78735c4fd133d643
JulieAnn Selman <julieannselman@gmail.com>
2023-03-15 17:25:05 -0400

    I made a dynamic progress bar that interacts with the start and end date. It does not look like much but it's bloody cool

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -6,10 +6,19 @@ class Offer < ApplicationRecord
     Offer.find_by(requester: first, collaborator: second) ||
       Offer.find_by(requester: second, collaborator: first)
   end
-
+  validates_presence_of :start_date, :end_date
   enum status: {
     pending: 0,
     accepted: 1,
     declined: 2
   }
+
+  def percent_time_left
+    return 100 if Time.now >= end_date
+
+    total_time = end_date - start_date
+    elapsed_time = Time.now - start_date
+    time_left = (elapsed_time * 100) / total_time
+    return time_left.round
+  end
 end
