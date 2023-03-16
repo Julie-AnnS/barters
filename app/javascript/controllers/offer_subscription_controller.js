1b35376490a87bac0fb472e6a8afbd04710f07b7
Nilsu Duran <duranilsu@gmail.com>
2023-03-16 01:17:31 -0400

    need to ask tmr

---
+++ app/javascript/controllers/offer_subscription_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
+// Connects to data-controller="insert-in-list"
+export default class extends Controller {
+  static targets = ["offers"]
+  static values = { currentUserId: Number }
+
+  connect() {
+    console.log(this.conversationIdValue);
+    this.channel = createConsumer().subscriptions.create(
+      { channel: 'OfferChannel', id: this.currentUserIdValue },
+      { received: data => this.#insertMessageAndScrollDown(data) }
+    )
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.itemsTarget.insertAdjacentHTML("afterbegin", data.notification)
+    this.numberTarget.innerText = Number(this.numberTarget.innerText) + 1
+    this.removezeroTarget.innerText = ""
+  }
+}
