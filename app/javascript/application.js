fce86a43e7b5b6d3f0abeea649b23300f61c370f
Nilsu Duran <duranilsu@gmail.com>
2023-03-22 16:27:00 -0400

    idk trying

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https') {
+    res.redirect(`https://${req.header('host')}${req.url}`)
+  } else {
+    next();
+  }
+});
+app.use(express.static('build'));
