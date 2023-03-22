8cf2f4631a89277b25e04c7a27be2c92794c8935
Nilsu Duran <duranilsu@gmail.com>
2023-03-22 16:33:37 -0400

    what

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,11 +4,11 @@ import "./controllers"
 import "bootstrap"
 
 
-app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https') {
-    res.redirect(`https://${req.header('host')}${req.url}`)
-  } else {
-    next();
-  }
-});
-app.use(express.static('build'));
+// app.use((req, res, next) => {
+//   if (req.header('x-forwarded-proto') !== 'https') {
+//     res.redirect(`https://${req.header('host')}${req.url}`)
+//   } else {
+//     next();
+//   }
+// });
+// app.use(express.static('build'));
